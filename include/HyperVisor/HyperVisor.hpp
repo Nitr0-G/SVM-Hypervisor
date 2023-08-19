c31f4a8c41754b753440312d4a05e0573e325d5d
Nitr0-G <120374383+Nitr0-G@users.noreply.github.com>
2023-08-19 17:50:17 +0300

    Include files

---
+++ include/HyperVisor/HyperVisor.hpp
@@ -0,0 +1,70 @@
+#pragma once 
+#include "CommonTypes/CPUID.hpp"
+#include "CommonTypes/MSR.hpp"
+#include "CommonTypes/Segmentation.hpp"
+#include "CommonTypes/SVM.hpp"
+#include "CommonTypes/Exception.hpp"
+#include "CommonTypes/Interrupts.hpp"
+#include "CommonApi/Callable.hpp"
+#include "CommonApi/MemoryUtils.hpp"
+
+#include <fltKernel.h>
+#include <intrin.h>
+
+struct GuestContext
+{
+	DWORD64 Rax; DWORD64 Rbx;
+	DWORD64 Rcx; DWORD64 Rdx;
+	DWORD64 Rsi; DWORD64 Rdi;
+	DWORD64 Rbp; DWORD64 R8;
+	DWORD64 R9;	DWORD64 R10;
+	DWORD64 R11; DWORD64 R12;
+	DWORD64 R13; DWORD64 R14;
+	DWORD64 R15;
+};
+
+extern "C" typedef void(*_SvmVmmRun)(
+	_In_ void* InitialVmmStackPointer);
+
+extern "C" typedef SVM::PRIVATE_VM_DATA* (*_Interceptions)(
+	_Inout_ SVM::PRIVATE_VM_DATA* Private);
+
+enum IntelEnc : unsigned int { IEbx = 'uneG', IEdx = 'Ieni', IEcx = 'letn' };
+enum AmdEnc : unsigned int { AEbx = 'htuA', AEdx = 'itne', AEcx = 'DMAc' };
+
+enum class CpuVendor { CpuUnknown, CpuIntel, CpuAmd};
+
+// Magic value, defined by hypervisor, triggers #VMEXIT and VMM shutdown:
+constexpr unsigned int HyperSign = 0x940049;
+constexpr unsigned int CPUID_VMM_SHUTDOWN = HyperSign;
+
+class HyperVisorSvm {
+private:
+	CpuVendor GetCpuVendor();
+	bool VirtualizeProcessor();
+	bool DevirtualizeProcessor(__out PVOID& PrivateVmData);
+
+	unsigned int RandomNumber();
+
+	PVOID AllocPhys(
+		_In_ SIZE_T Size,
+		_In_ MEMORY_CACHING_TYPE CachingType,
+		_In_ ULONG MaxPhysBits);
+
+	void BuildNestedPagingTables(
+		__out SVM::NESTED_PAGING_TABLES* Npt);
+
+	void FillVmcbSegmentAttributes(
+		_Out_ SVM::VMCB_STATE_SAVE_AREA::VMCB_SEGMENT_ATTRIBUTE* Attribute,
+		_In_ const SEGMENT_SELECTOR* Selector,
+		_In_ const DESCRIPTOR_TABLE_REGISTER_LONG* Gdtr);
+private:
+	static inline volatile bool g_IsVirtualized;
+	_SvmVmmRun SvmVmmRun; _Interceptions Interceptions;
+public:
+	PVOID PSvmVmmRun = nullptr, PInterceptions = nullptr;
+public:
+	bool DevirtualizeAllProcessors();
+	bool VirtualizeAllProcessors();
+	bool IsSvmSupported();
+};
